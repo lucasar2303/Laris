9e3a5b82d6ad3645f0eb31bf197b20e2252ac698
Lucas Anastácio <lucas-ar13@hotmail.com>
2022-09-28 16:14:49 -0300

    Layout Login + Verificação senha e cpf + mascara cpf

---
+++ app/src/main/java/com/example/laris/LoginActivity.java
@@ -0,0 +1,80 @@
+package com.example.laris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.laris.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    private Button btnEntrar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.btnEntrar.setOnClickListener(view -> verificaDados());
+        binding.imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WelcomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void verificaDados(){
+        Boolean cpfValido = binding.etCpf.isDone();
+        Boolean senhaValida = false;
+        String senha = binding.etSenha.getText().toString();
+
+        if (!senha.equals("")){
+            senhaValida = true;
+        }else{
+            senhaValida = false;
+
+        }
+
+        if (cpfValido && senhaValida){
+            //Chamar banco
+        }else{
+            binding.layoutCPF.setBoxStrokeColor(Color.parseColor("#7C4EFF"));
+            binding.layoutSenha.setBoxStrokeColor(Color.parseColor("#7C4EFF"));
+            binding.tvErro.setVisibility(View.GONE);
+            if (!cpfValido && !senhaValida){
+                binding.tvErro.setVisibility(View.VISIBLE);
+                binding.tvErro.setText("Digite suas credenciais");
+                binding.layoutCPF.setBoxStrokeColor(Color.parseColor("#FF0000"));
+                binding.layoutSenha.setBoxStrokeColor(Color.parseColor("#FF0000"));
+                binding.etCpf.requestFocus();
+            }else{
+
+            if (!senhaValida){
+                binding.tvErro.setVisibility(View.VISIBLE);
+                binding.tvErro.setText("Digite sua senha");
+                binding.layoutSenha.setBoxStrokeColor(Color.parseColor("#FF0000"));
+                binding.etSenha.requestFocus();
+            }
+            if (!cpfValido){
+                binding.tvErro.setVisibility(View.VISIBLE);
+                binding.tvErro.setText("Digite um CPF válido");
+                binding.layoutCPF.setBoxStrokeColor(Color.parseColor("#FF0000"));
+                binding.etCpf.requestFocus();
+            }}
+
+        }
+
+
+    }
+
+}
--- app/src/main/java/com/example/laris/WelcomeActivity.java
+++ app/src/main/java/com/example/laris/WelcomeActivity.java
@@ -2,13 +2,31 @@ package com.example.laris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.example.laris.databinding.ActivityWelcomeBinding;
 
 public class WelcomeActivity extends AppCompatActivity {
+
+    private ActivityWelcomeBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome);
+        binding = ActivityWelcomeBinding.inflate(getLayoutInflater());
+        setTheme(R.style.Theme_Laris);
+        setContentView(binding.getRoot());
+
+        binding.btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
